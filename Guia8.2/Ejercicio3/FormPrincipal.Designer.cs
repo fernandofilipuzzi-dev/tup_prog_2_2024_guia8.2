acbe9eae305703dbe67ff9409c4a179e9664ade6
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-09-29 04:47:50 -0300

    .

--- Guia8.2/Ejercicio3/FormPrincipal.Designer.cs
+++ Guia8.2/Ejercicio3/FormPrincipal.Designer.cs
@@ -28,20 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbVer = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 103);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Prueba";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnPrueba_Click);
+            // 
+            // tbVer
+            // 
+            this.tbVer.Location = new System.Drawing.Point(13, 14);
+            this.tbVer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbVer.Multiline = true;
+            this.tbVer.Name = "tbVer";
+            this.tbVer.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbVer.Size = new System.Drawing.Size(406, 346);
+            this.tbVer.TabIndex = 1;
+            this.tbVer.TextChanged += new System.EventHandler(this.tbVer_TextChanged);
             // 
             // FormPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(461, 310);
+            this.ClientSize = new System.Drawing.Size(560, 368);
+            this.Controls.Add(this.tbVer);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormPrincipal";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbVer;
     }
 }
 
---
+++ Guia8.2/Ejercicio3/Models/Cuota.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Ejercicio3.Models
+{
+    public class Cuota
+    {
+        public int Numero { get; private set;  }
+        public double ValorAPagar { get; private set; }
+        public DateTime FechaPago { get; private set; }
+
+        public Cuota(int numero,  double valorAPagar, DateTime fechaPago)
+        {
+            ValorAPagar = valorAPagar;
+            FechaPago = fechaPago;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numero:000}    {FechaPago:dd/MM/yyyy}    ${ValorAPagar:f2}";
+        }
+    }
+}
